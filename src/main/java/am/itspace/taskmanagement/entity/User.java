98bf209c8bf731d9928ed96ac28b101e7fc3be5c
hasmiksarhatyan <hasmiks88.hs@gmail.com>
2022-09-28 16:00:44 +0400

    taskmanagment

---
+++ src/main/java/am/itspace/taskmanagement/entity/User.java
@@ -0,0 +1,26 @@
+package am.itspace.taskmanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="user")
+public class User  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+    private String picUrl;
+}
