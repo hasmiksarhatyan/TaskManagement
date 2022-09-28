9527fe47e055f248728444241a7660e5c3f08643
hasmiksarhatyan <hasmiks88.hs@gmail.com>
2022-09-28 17:20:10 +0400

    taskmanagment

---
+++ src/main/java/am/itspace/taskmanagement/controller/TaskController.java
@@ -0,0 +1,64 @@
+package am.itspace.taskmanagement.controller;
+
+import am.itspace.taskmanagement.entity.Task;
+import am.itspace.taskmanagement.entity.User;
+import am.itspace.taskmanagement.repository.TaskRepository;
+import am.itspace.taskmanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Controller
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/tasks")
+    public String tasks(ModelMap modelMap) {
+        List<Task> allTasks = taskRepository.findAll();
+        List<User> allUsers = userRepository.findAll();
+        modelMap.addAttribute("tasks", allTasks);
+        modelMap.addAttribute("users", allUsers);
+        return "tasks";
+    }
+    @PostMapping("/tasks/changeUser")
+    public String changeUser(@RequestParam("userId") int userId, @RequestParam("taskId")int taskId){
+        Optional<Task> taskOptional = taskRepository.findById(taskId);
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (taskOptional.isPresent() && userOptional.isPresent()){
+            Task task = taskOptional.get();
+            User user = userOptional.get();
+            if (task.getUser()!=user){
+                task.setUser(user);
+                taskRepository.save(task);
+            } else if (taskOptional.isPresent()&&userId==0) {
+                taskOptional.get().setUser(null);
+                taskRepository.save(taskOptional.get());
+            }
+        }
+        return "redirect:/tasks";
+    }
+
+    @GetMapping("/tasks/add")
+    public String addTaskPage(ModelMap modelMap) {
+        List<User> users = userRepository.findAll();
+        modelMap.addAttribute("users", users);
+        return "addTask";
+    }
+
+    @PostMapping("/tasks/add")
+    public String addTask(@ModelAttribute Task task) {
+        taskRepository.save(task);
+        return "redirect:/tasks";
+    }
+}
---
+++ src/main/java/am/itspace/taskmanagement/controller/UserController.java
@@ -0,0 +1,61 @@
+package am.itspace.taskmanagement.controller;
+
+import am.itspace.taskmanagement.entity.User;
+import am.itspace.taskmanagement.repository.UserRepository;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.*;
+import java.util.List;
+
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @Value("${task.management.images.folder}")
+    private String folderPath;
+
+    @GetMapping("/users")
+    public String users(ModelMap modelMap) {
+        List<User> allUsers = userRepository.findAll();
+        modelMap.addAttribute("users", allUsers);
+        return "users";
+    }
+
+    @GetMapping("/users/add")
+    public String addUserPage() {
+
+        return "addUser";
+    }
+
+    @PostMapping("/users/add")
+    public String addUser(@ModelAttribute User user,
+                          @RequestParam("userImage") MultipartFile file) throws IOException {
+        if (!file.isEmpty() && file.getSize() > 0) {
+            String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+            File newFile = new File(folderPath + File.separator + fileName);
+            file.transferTo(newFile);
+            user.setPicUrl(fileName);
+        }
+        userRepository.save(user);
+        return "redirect:/users";
+    }
+    @GetMapping(value = "/users/getImage",produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImage(@RequestParam("fileName") String fileName) throws IOException {
+        InputStream inputStream=new FileInputStream(folderPath+File.separator+fileName);
+        return IOUtils.toByteArray(inputStream);
+    }
+    @GetMapping("/users/delete")
+    public String delete(@RequestParam("id") int id){
+        userRepository.deleteById(id);
+        return "redirect:/users";
+    }
+}
